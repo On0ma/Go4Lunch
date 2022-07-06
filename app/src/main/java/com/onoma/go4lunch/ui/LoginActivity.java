499d12e4aa002e63b6bf91d4f7d6ebf6f91b66a5
onoma <edeverne@gmail.com>
2022-07-06 16:50:47 +0200

    Renamed login and main activities

---
+++ app/src/main/java/com/onoma/go4lunch/ui/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.onoma.go4lunch.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+import com.onoma.go4lunch.R;
+import com.onoma.go4lunch.ViewModelFactory;
+import com.onoma.go4lunch.databinding.ActivityMainBinding;
+import com.onoma.go4lunch.ui.viewModel.UserViewModel;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LoginActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final int RC_SIGN_IN = 123;
+    private UserViewModel mUserViewModel;
+
+    @Override
+    ActivityMainBinding getViewBinding() {
+        return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(UserViewModel.class);
+        setupListeners();
+        if (mUserViewModel.isCurrentUserLogged()) {
+            startMapActivity();
+        }
+    }
+
+    private void setupListeners(){
+        // Login Button
+        binding.loginButtonGoogle.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    private void startSignInActivity(){
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers =
+                Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void startMapActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                showSnackBar(getString(R.string.connection_succeed));
+                startMapActivity();
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError()!= null) {
+                    if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+            }
+        }
+    }
+
+}
